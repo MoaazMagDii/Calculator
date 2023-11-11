583aa5f39ad3fbd2b8515d76a1958d1620102b8e
MoaazMagDii <moaazmagdy79@gmail.com>
2023-11-11 22:32:38 +0200

    adding files

---
+++ app/src/main/java/com/example/assignment2/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.assignment2
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var btnSendData : Button
+    private lateinit var editUserName : EditText
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnSendData = findViewById(R.id.btn_sendData)
+        editUserName = findViewById(R.id.et_username)
+
+        btnSendData.setOnClickListener {
+            startActivity(Intent(this,SecondActivity::class.java)
+                .putExtra("username",editUserName.text.toString()))
+        }
+
+
+
+    }
+}
---
+++ app/src/main/java/com/example/assignment2/SecondActivity.kt
@@ -0,0 +1,36 @@
+package com.example.assignment2
+
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class SecondActivity : AppCompatActivity() {
+
+    private lateinit var textUserName : TextView
+    private lateinit var btnStart : Button
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+
+
+        textUserName = findViewById(R.id.tv_username)
+
+        val userName = intent.getStringExtra("username")
+
+        textUserName.text = "Hello, "+userName
+
+
+        btnStart = findViewById(R.id.btn_Start)
+        btnStart.setOnClickListener {
+            startActivity(
+                Intent(this, ThirdActivity::class.java)
+            )
+        }
+
+    }
+}
